a63ba66a645f63c36e326127aa453a30dea5056c
Arman176001 <armanchaudhary176001@gmail.com>
2024-12-03 22:22:43 +0530

    check

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from flask_cors import CORS
 from googleapiclient.discovery import build
 from groq import Groq
 from youtube_transcript_api import YouTubeTranscriptApi
-from dotenv import load_dotenv
-load_dotenv()
+
 def create_app():
     app = Flask(__name__)
     CORS(app)  # Enable CORS for all routes
